85ff175c412e3a7c5f82e26f1c2e6e62e260cfcb
Diego Quintanilla <diego.quintanilla@proton.me>
2023-07-29 13:56:58 -0400

    add task related components and setup services folder

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+import { Task } from '../../interfaces/Task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css'],
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task = {
+    text: '',
+    day: '',
+    reminder: false,
+  };
+
+  faTimes = faTimes;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
